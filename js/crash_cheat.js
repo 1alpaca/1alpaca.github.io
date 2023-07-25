fc9cd51f77283ab58aff9339649f15293b15768b
1alpaca <qiuwenjing1002@163.com>
2023-07-25 15:07:30 +0800

    Site updated: 2023-07-25 15:07:29

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -3,11 +3,11 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         document.title = '！！这里这里 ';
+         document.title = '这里这里Σ(/ﾟДﾟ)/！！ ';
          clearTimeout(titleTime);
      }
      else {
-         document.title = '安全屋继续运行！！' + OriginTitle;
+         document.title = '安全屋继续运行！！(・ω< )★' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
